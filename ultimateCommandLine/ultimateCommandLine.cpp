8845180605a3b56229b0f93aecf1ce8b7a88337e
Jawaad Ramcharitar <jawaadramcharitar@gmail.com>
2025-06-28 16:40:38 -0400

    Main function started

--- ultimateCommandLine/ultimateCommandLine.cpp
+++ ultimateCommandLine/ultimateCommandLine.cpp
@@ -2,8 +2,34 @@
 /* YS - 06 / 28 / 2025 - Added Change Log */
 
 #include <iostream>
+#include <filesystem>
+#include<string>
+#include<sstream>
+#include<vector>
 
 int main() {
+
+    while (true) {
+
+        std::string input;
+        std::filesystem::path currentPath = std::filesystem::current_path();
+
+        std::cout << currentPath << "> ";
+        std::cin >> input;
+
+        std::stringstream ss(input);
+        std::vector<std::string> parsedInput;
+        std::string token;
+
+        while (ss >> token) {
+            parsedInput.push_back(token);
+        }
+
+        std::cout << "Hello World!\n";
+
+    }
+
+
     std::cout << "Hello World!\n";
     return 0;
 }
