0584d2043297c93d02dc9956bce3eb46f09978ab
Yusuf <yusufsattar@gmail.com>
2025-06-30 23:43:53 -0400

    Added change directory function

--- ultimateCommandLine/commands.cpp
+++ ultimateCommandLine/commands.cpp
@@ -2,6 +2,9 @@
 /* YS - 06 / 29 / 2025 - Initial Creation */
 
 #include "commands.h"
+#include "globals.h"
+
+using namespace GLOBALS;
 
 void listFiles(std::vector <std::string>& fullCmd) {
 
@@ -17,5 +20,32 @@ void listFiles(std::vector <std::string>& fullCmd) {
 }
 
 void changeDirectory(std::vector <std::string>& fullCmd) {
+
+	//check if only "cd" so base case or too many arguments
+	if (fullCmd.size() != 2) {
+		//only "cd or too many commands so exit early to save processing
+		return;
+	}
+	else {
+		if (!std::filesystem::exists(fullCmd[1]) || !std::filesystem::is_directory(fullCmd[1])) {
+			//path doesn't exist or is not a directory
+			return;
+		}
+		else {
+			if (fullCmd[1] == "..") {
+				// go back a directory and set to workingDir
+				return;
+			}
+			else if (fullCmd[1] == ".") {
+				// go back to parent directory
+				return;
+			}
+			else {
+				GLOBALS::set_workingDir(fullCmd[1]);
+				return;
+			}
+		}
+	}
+
 	return;
 }
